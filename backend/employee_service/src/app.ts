9e7230e0c67f8851df1eda38b28dd05570bb1fc7
chechu776 <shameerchechu@gmail.com>
2025-12-15 22:40:37 +0530

    admin table and info added in db

---
+++ backend/employee_service/src/app.ts
@@ -0,0 +1,24 @@
+import "reflect-metadata";
+import express from "express";
+import { Source } from "./config/dataSource";
+import "./config/env";
+
+const app = express();
+app.use(express.json());
+
+const startServer = async () => {
+  try {
+    await Source.initialize();
+    console.log("Database connected");
+
+    const PORT = process.env.PORT;
+    app.listen(PORT, () => {
+      console.log(`Server running on port ${PORT}`);
+    });
+  } catch (error) {
+    console.error("DB connection failed", error);
+    process.exit(1);
+  }
+};
+
+startServer();
---
+++ backend/employee_service/src/entities/adminEntities.ts
@@ -0,0 +1,16 @@
+import { Entity, PrimaryGeneratedColumn, Column, CreateDateColumn } from "typeorm";
+
+@Entity("admin")
+export class Admin {
+    @PrimaryGeneratedColumn("uuid")
+    id!: string;
+
+    @Column({ type: "varchar", length: 255 })
+    email!: string;
+
+    @Column({ type: "varchar", length: 255 })
+    passwordHash!: string;
+
+    @CreateDateColumn({ type: "timestamp with time zone" })
+    createdAt!: Date;
+}
