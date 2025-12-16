ee492f121f2b385158b01b566afb6618037de19f
riyas <mriyastk8@gmail.com>
2025-12-16 11:18:51 +0530

    next app configuration,shard cn installation,login page completed

--- frontend/app/login/page.tsx
+++ frontend/app/login/page.tsx
@@ -2,6 +2,7 @@ import { LoginForm } from "@/components/login-form"
 
 export default function LoginPage() {
   return (
+    <>
     <div className="grid min-h-svh lg:grid-cols-2">
       <div className="flex flex-col gap-4 p-6 md:p-10">
         <div className="flex flex-1 items-center justify-center">
@@ -19,5 +20,6 @@ export default function LoginPage() {
         />
       </div>
     </div>
+    </>
   )
 }
