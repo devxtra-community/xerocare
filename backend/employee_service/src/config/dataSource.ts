642db77f30d00ec9d1caef5a399c913a802ec72e
chechu776 <shameerchechu@gmail.com>
2025-12-16 14:33:15 +0530

    changed to neon db

--- backend/employee_service/src/config/dataSource.ts
+++ backend/employee_service/src/config/dataSource.ts
@@ -10,11 +10,10 @@ import { Branch } from "../entities/branchEntities";
 
 export const Source = new DataSource({
   type: "postgres",
-  host: process.env.DB_HOST,
-  port: Number(process.env.DB_PORT),
-  username: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
+  url:process.env.DATABASE_URL,
+  ssl:{
+    rejectUnauthorized:false,
+  },
   synchronize: true,
   entities: [Admin,Employee,Auth,Otp,Branch],
 });
