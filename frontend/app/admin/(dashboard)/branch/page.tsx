c96e33d7d4338513fc48ccd0e0956e4638b682e4
nadhil7 <nadhunadhil33429@gmail.com>
2026-01-03 12:07:06 +0530

    fix : lint error

--- frontend/app/admin/(dashboard)/branch/page.tsx
+++ frontend/app/admin/(dashboard)/branch/page.tsx
@@ -1,6 +1,6 @@
-import next from 'next';
 import BranchReport from '@/components/AdminDahboardComponents/branchComponents/BranchTable';
 
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 export default function BranchPage() {
   return (
     <>
