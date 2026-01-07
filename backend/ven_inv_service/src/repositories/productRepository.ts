09f9a74f0bba68873dc9412b08c2c3e01d25e866
nadhil7 <nadhunadhil33429@gmail.com>
2026-01-07 11:35:39 +0530

    fix: in product entity and repository  based on some operations

--- backend/ven_inv_service/src/repositories/productRepository.ts
+++ backend/ven_inv_service/src/repositories/productRepository.ts
@@ -1,42 +1,52 @@
-import { Source } from "../config/db";
-import { Product } from "../entities/productEntity";
+import { Source } from '../config/db';
+import { Product } from '../entities/productEntity';
 
 export class ProductRepository {
-    private repo = Source.getRepository(Product);
+  private repo = Source.getRepository(Product);
 
-    async addProduct(data: any) {
-        const product = this.repo.create(data);
-        return this.repo.save(product);
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  async addProduct(data: any) {
+    if (data.model_id) {
+      data.model = { id: data.model_id };
+      delete data.model_id;
     }
+    const product = this.repo.create(data);
+    return this.repo.save(product);
+  }
 
-    async getAllProducts() {
-        return this.repo.find({
-            relations: {
-                model_id: true
-            }
-        });
-    }
+  async getAllProducts() {
+    return this.repo.find({
+      relations: {
+        model: true,
+      },
+    });
+  }
 
-    async updateProduct(id: string, data: any) {
-        await this.repo.update(id, data);
-        return this.repo.findOne({
-            where: { id },
-            relations: {
-                model_id: true
-            }
-        });
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  async updateProduct(id: string, data: any) {
+    if (data.model_id) {
+      data.model = { id: data.model_id };
+      delete data.model_id;
     }
+    await this.repo.update(id, data);
+    return this.repo.findOne({
+      where: { id },
+      relations: {
+        model: true,
+      },
+    });
+  }
 
-    async deleteProduct(id: string) {
-        return this.repo.delete(id);
-    }
+  async deleteProduct(id: string) {
+    return this.repo.delete(id);
+  }
 
-    async findOne(id: string) {
-        return this.repo.findOne({
-            where: { id },
-            relations: {
-                model_id: true
-            }
-        });
-    }
+  async findOne(id: string) {
+    return this.repo.findOne({
+      where: { id },
+      relations: {
+        model: true,
+      },
+    });
+  }
 }
